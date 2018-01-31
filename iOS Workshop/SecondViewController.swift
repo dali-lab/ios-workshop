2b59f9be53a9438d05c80676eceb3073ad3a8fd7
John Kotz <john.lyme@mac.com>
2018-01-30 22:48:55 -0500

    Initial Commit

---
+++ iOS Workshop/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS Workshop
+//
+//  Created by John Kotz on 1/30/18.
+//  Copyright © 2018 You!. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
