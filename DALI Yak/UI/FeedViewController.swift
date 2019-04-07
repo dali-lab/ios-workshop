0d4a3ea5bcc011cfce33ecb287dbd27d17a21e68
John Kotz <john.lyme@mac.com>
2019-04-07 17:33:25 -0400

    Adding app icon, restructuring, and adding Firebase

---
+++ DALI Yak/UI/FeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewController.swift
+//  DALI Yak
+//
+//  Created by John Kotz on 4/7/19.
+//  Copyright © 2019 You. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedViewController: UITableViewController {
+    
+}
---
+++ DALI Yak/UI/FeedViewControllerCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewControllerCell.swift
+//  DALI Yak
+//
+//  Created by John Kotz on 4/7/19.
+//  Copyright © 2019 You. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedViewControllerCell: UITableViewCell {
+    
+}
